29845ae9c9b109eb62af8bfd81654e3189ed1c3d
Rosmery Aruni Paye <145420260+RossPagget@users.noreply.github.com>
2026-03-16 23:39:02 -0400

    Refactor comments and maintain functionality

--- HOLAMUNDO_RAP_17-03-2026/Ejercicio_2.py
+++ HOLAMUNDO_RAP_17-03-2026/Ejercicio_2.py
@@ -1,6 +1,5 @@
 #Ejercicio 2
-#Nombre: Rosmery Aruni Paye
-#CI: 9244293        RU:200075568       
+#Nombre: Rosmery Aruni Paye  RU:200075568       
 #Materia: Tecnologias Emergentes II
 
 # entrada de datos
