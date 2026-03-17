06226d9957712ad76bf1bc97470c11cb2d816860
Rosmery Aruni Paye <145420260+RossPagget@users.noreply.github.com>
2026-03-16 23:38:04 -0400

    Format and clean up Ejercicio_11.py comments

--- HOLAMUNDO_RAP_17-03-2026/Ejercicio_11.py
+++ HOLAMUNDO_RAP_17-03-2026/Ejercicio_11.py
@@ -1,6 +1,5 @@
 #Ejercicio 11
-#Nombre: Rosmery Aruni Paye
-#CI: 9244293        RU:200075568       
+#Nombre: Rosmery Aruni Paye   RU:200075568       
 #Materia: Tecnologias Emergentes II
 
 #tuplas (inmutables)
